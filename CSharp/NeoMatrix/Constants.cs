317b900d5807c8f6299c98b24d17f08514993d52
LeaFrock <petergeorge@126.com>
2020-04-21 23:32:54 +0800

    Add global Constants

---
+++ CSharp/NeoMatrix/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace NeoMatrix
+{
+    public static class Constants
+    {
+        public const string MAIN_NET = "MainNet";
+        public const string TEST_NET = "TestNet";
+        public const string PRIV_NET = "PrivateNet";
+    }
+}
